88b2fcc1697be93d4d07612931062e4d9b23055a
git-felipeUrtubia <felipe.urtubia7@gmail.com>
2026-01-28 07:16:09 -0300

    api-gateway: Agregada ruta vercel del frontend

--- catalog-service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/example/catalogservice/CatalogServiceApplication.java
@@ -2,10 +2,10 @@ package com.example.catalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+
+@SpringBootApplication
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
