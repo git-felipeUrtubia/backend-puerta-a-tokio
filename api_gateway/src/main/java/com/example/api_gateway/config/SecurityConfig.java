bc82d0734a5a382e5b4d78158f4fc5c9095f9ace
git-felipeUrtubia <felipe.urtubia7@gmail.com>
2026-01-29 04:47:35 -0300

    api-gateway: Agregada ruta vercel del frontend

--- api_gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
+++ api_gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOriginPattern("https://puerta-a-tokio-front.vercel.app");
+        configuration.addAllowedOriginPattern("https://puerta-a-tokio-front.vercel.app/home");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
